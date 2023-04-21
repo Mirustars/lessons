34ecc9b9dbe6979be7b63819ba2ad479cfcc80f7
Mirustars <109741153+Mirustars@users.noreply.github.com>
2023-04-21 14:36:42 +0300

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,30 @@
+
+const putin = "huylo";
+console.log(Boolean(putin));
+console.log(putin);
+
+let countryName = "Canada"
+console.log(typeof countryName);
+console.log(countryName);
+
+let humanHeight = 183;
+console.log(typeof humanHeight);
+console.log(humanHeight);
+
+let result = 99 / 3;
+console.log(result);
+
+let someTerrorist = `putin ...`;
+someTerrorist = someTerrorist.replace('...', 'huylo');
+console.log(someTerrorist);
+
+
+let varOne = "putin ";
+let varTwo = "terrorist";
+let varSum = varOne + varTwo;
+console.log(varSum);
+
+
+let userName;
+//Отримаємо тип даних Null
+console.log(typeof userName);
